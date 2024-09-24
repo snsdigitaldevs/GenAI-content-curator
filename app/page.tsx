0fe9db6ccd3363dbc6a31f5719f0f52c8597ddb6
NiFang <ni.fang@thoughtworks.com>
2024-09-24 11:26:42 +0800

    feat(AAP-1 - ni): update page.tsx for imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import { Authenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
-
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
-Amplify.configure(outputs);
+// only use for local development
+// import outputs from "@/amplify_outputs.json";
+// import { Amplify } from "aws-amplify";
+
+// Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
